ef7971fe15da6605a67658496fe2aaeef9b96361
Alison Ostendorf <aostendorf@gmail.com>
2017-04-20 14:54:01 -0400

    fix a missing comment opener, add params permit, clean up orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,18 +11,20 @@ class OrdersController < ApplicationController
 		@order = Order.new(order_params)
 
 		if @order.save
-			format.html {}
+			format.html { render :show}
 		else
-			format.html {}
+			format.html { render :new }
 		end
 	end 
 
-	# will need to create a create action
+	def show 
+		# show the receipt from the order
+	end
 
 private
 	
 	def order_params
-		params.require(:order).permit(:)
+		params.require(:order).permit(:contact_name, :contact_company_name, :contact_email, :contact_phone_number, :project_type, :project_name, :project_timeline, :additional_comments, :text_preferred, :preference_no_call)
 	end
 
 end
